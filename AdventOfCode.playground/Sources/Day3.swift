0bf78c3f65a645cf329bf33371727592bf742fde
Alex Stroulger <alexstroulger@gmail.com>
2017-12-20 21:53:05 -0800

    Started day 3

---
+++ AdventOfCode.playground/Sources/Day3.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+//
+// get the dimensions of the array based on the square root rounded up
+// i.e. round up sqrt(265149) and that tells you length and height
+// then figure out where the middle is based on the dimensions
+// 
